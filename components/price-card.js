667d01dda1670f1e68678d1bf6982ac818662fc5
Punith K <kpunith8@gmail.com>
2022-01-30 18:53:31 +0530

    Add total and percentage and refactoring

--- components/price-card.js
+++ components/price-card.js
@@ -2,47 +2,86 @@ import { useMemo } from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import { round } from 'lodash'
 import allSymbols from '../utils/static/symbols-data';
-import Divider from '@mui/material/Divider';
 import Image from 'next/image';
 import { makeStyles } from '@mui/styles';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
-const useStyles = makeStyles((theme) => ({
-  cardItem: {
-    padding: theme.spacing(2),
-  },
-}))
+// Custom styles
+// const useStyles = makeStyles((theme) => ({
+//   cardItem: {
+//     padding: theme.spacing(2),
+//   },
+// }))
+
+const findSymbol = (symbol) => allSymbols.find(item => item.symbol === symbol)
+
+const PriceItem = ({ label, total, percentage }) => (
+  <div style={{ display: 'flex', fontSize: 13 }}>
+    <span>{`${label}:`}</span>
+    <span style={{ color: total > 0 ? 'green' : 'red', marginLeft: 10 }}>
+      <div style={{ display: 'flex' }}>
+        <span style={{ marginTop: -2 }}>{total > 0 ? <ArrowDropUpIcon sx={{ color: 'green' }} /> : <ArrowDropDownIcon sx={{ color: 'red' }} />}</span>
+        <span>{`${total > 0 ? `+${total}` : `${total}`} (${percentage}%)`}</span>
+      </div>
+    </span>
+  </div>
+)
 
 const PriceCard = ({ data, inrPrice }) => {
   const symbol = useMemo(() => data.symbol.slice(0, -4), [data.symbol])
-  const symbolIcon = useMemo(() => allSymbols.find(item => item.symbol === symbol).icon, [symbol])
-  const symbolName = useMemo(() => allSymbols.find(item => item.symbol === symbol).name, [symbol])
-  const totalPortfolio = useMemo(() => {
-    const symbolFound = allSymbols.find(item => item.symbol === symbol)
-    const totalInvestment = symbolFound.totalCoins * symbolFound.avgBuyPrice
-    const currentPrice = round(inrPrice * data.currentPrice * symbolFound.totalCoins, 4)
+  const symbolData = useMemo(() => {
+    const foundSymbol = findSymbol(symbol)
+    return {
+      name: foundSymbol.name,
+      icon: foundSymbol.icon,
+    }
+  }, [symbol])
 
-    return round(currentPrice - totalInvestment, 2)
+  const totalPortfolio = useMemo(() => {
+    const symbolFound = findSymbol(symbol)
+    const totalInvestment = symbolFound.totalCoins * symbolFound.avgBuyPrice
+    const currentPrice = round(inrPrice * data.currentPrice * symbolFound.totalCoins, 2)
+    const total = round(currentPrice - totalInvestment, 2)
+    const percentage = round(total / totalInvestment * 100, 2)
+
+    return {
+      total,
+      percentage
+    }
   }, [symbol, data.currentPrice, inrPrice])
 
-  const percentageMove = useMemo(() => round((data.currentPrice - data.closePrice) / data.currentPrice * 100, 2), [data.currentPrice, data.closePrice])
-  const classes = useStyles()
+  const last24HrChange = useMemo(() => {
+    const symbolFound = findSymbol(symbol)
+    const { currentPrice, closePrice } = data
+    const lastPrice = round(inrPrice * currentPrice * symbolFound.totalCoins, 2)
+    const total = round(lastPrice - closePrice * inrPrice * symbolFound.totalCoins, 2)
+    const percentage = round((currentPrice - closePrice) / currentPrice * 100, 2)
+
+    return {
+      total,
+      percentage
+    }
+  }, [data, symbol, inrPrice])
+
+  console.log({ last24HrChange })
+
   return (
     <Card>
       <CardHeader
         avatar={
           <Avatar aria-label="recipe">
-            {symbolIcon && <Image src={symbolIcon} />}
+            {symbolData.icon && <Image src={symbolData.icon} />}
           </Avatar>
         }
         title={symbol}
-        subheader={symbolName}
+        subheader={symbolData.name}
       />
-      <CardContent style={{paddingBottom: 16}}>
+      <CardContent style={{ paddingBottom: 16 }}>
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
           <div>
             <Typography variant="body2" color="text.secondary">
@@ -53,18 +92,8 @@ const PriceCard = ({ data, inrPrice }) => {
             </Typography>
           </div>
           <div style={{ display: 'flex', alignItems: 'space-between', flexDirection: 'column' }}>
-            <div style={{ fontSize: 13, marginBottom: 5 }}>
-              <span>24H:</span>
-              <span style={{ color: percentageMove > 0 ? 'green' : 'red', marginLeft: 10 }}>
-                {`${percentageMove > 0 ? `+${percentageMove}` : `${percentageMove}`}%`}
-              </span>
-            </div>
-            <div style={{ fontSize: 13 }}>
-              <span>Total:</span>
-              <span style={{ color: totalPortfolio > 0 ? 'green' : 'red', marginLeft: 10 }}>
-                {`${totalPortfolio > 0 ? `+${totalPortfolio}` : `${totalPortfolio}`}`}
-              </span>
-            </div>
+            <PriceItem label="24Hr" total={last24HrChange.total} percentage={last24HrChange.percentage} />
+            <PriceItem label="Total" total={totalPortfolio.total} percentage={totalPortfolio.percentage} />
           </div>
         </div>
       </CardContent>
